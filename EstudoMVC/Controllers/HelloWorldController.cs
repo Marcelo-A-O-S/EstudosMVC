c3e8fb75ee82ba3e72964a90fa9e54bf0d21649f
Marcelo Augusto <77033790+Marcelo-A-O-S@users.noreply.github.com>
2022-12-23 01:43:14 -0300

    Commit

---
+++ EstudoMVC/Controllers/HelloWorldController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using EstudoMVC.Models;
+
+namespace EstudoMVC.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        private static List<DogViewModel> dogs = new List<DogViewModel>();
+        public IActionResult Index()
+        {
+            return View(dogs);
+        }
+        public IActionResult Create() 
+        {
+            var DogVm = new DogViewModel();
+            return View(DogVm);
+        }
+        public IActionResult CreateDog(DogViewModel dog) 
+        {
+            dogs.Add(dog);
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
